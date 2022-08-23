bbf4207144511a7bd7d9d76c85acd4317e15b17d
0xKoda <koda_0x@protonmail.com>
2022-08-23 00:16:55 -0400

    add endpoint

--- script/Medusa.s.sol
+++ script/Medusa.s.sol
@@ -1,12 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {Surl} from "surl/Surl.sol";
+import "../src/Medusa.sol";
 
-contract ContractScript is Script {
-    function setUp() public {}
-
-    function run() public {
-        vm.broadcast();
+contract MedusaScript is Script {
+    using Surl for *;
+    // Medusa medusa ;
+    
+    function run() external {
+         vm.startBroadcast();
+        (uint256 status, bytes memory data) = "https://tutela.xyz/search?address=0xdd140cb2847f6186a9df3b25ae1e135052b09321".get();
+        // bytes memory res = cluster;
+        console.log("status", status);
+        console.log("data", string(data));
+        vm.stopBroadcast();
     }
 }
+
+
--- src/Medusa.sol
+++ src/Medusa.sol
@@ -1,4 +1,45 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Medusa {}
+contract Medusa {
+    address owner;
+    uint256 root;
+    uint256 head;
+
+    mapping(address => uint256) web; //Map address to CID
+    // mapping(uint256 => uint256) dag; //Map dag root to top CID
+    event newHead(uint256);
+    event newRoot(uint256);
+
+    constructor(){
+        owner = msg.sender;
+        root = 0;
+    }
+
+// state changers
+//set new root
+    function sR(uint256 i) public returns(uint256){
+        root = i;
+        emit newRoot(i);
+        return i;
+    }
+//attatch new head
+    function sH(uint256 i) public returns(uint256){
+        require(root != 0, "cant attatch head without root");
+        head = i;
+        emit newHead(i);
+        return i;
+
+    }
+//add new address to stone
+    function add(address u, uint256 cid) public{
+        web[u] = cid;
+    }
+
+    // public endpoints 
+
+//search web
+    function _sw(address c) public view returns(bool){
+        return web[c] > 0 ? true : false;
+    }
+}
